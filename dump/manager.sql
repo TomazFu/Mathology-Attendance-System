7306314e079baf3e22a3240305acf21b55cc97ca
lhk01 <hkleong89@gmail.com>
2024-11-08 11:25:59 +0800

    minor changes

--- dump/manager.sql
+++ dump/manager.sql
@@ -21,6 +21,10 @@ SET time_zone = "+00:00";
 -- Database: `mathlogydb`
 --
 
+CREATE DATABASE IF NOT EXISTS mathlogydb;
+
+USE mathlogydb;
+
 -- --------------------------------------------------------
 
 --
