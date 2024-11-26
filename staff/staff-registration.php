45e62c08315fe3aa9f320f8989bb76bd5d8e5db9
lihspang <lihspang@gmail.com>
2024-11-26 23:17:50 +0800

    password confirm update

--- staff/staff-registration.php
+++ staff/staff-registration.php
@@ -20,7 +20,7 @@ require_once "../includes/sidebar.php";
 <!DOCTYPE html>
 <html>
 
-<head> 
+<head>
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/staff.css">
 </head>
@@ -31,15 +31,15 @@ require_once "../includes/sidebar.php";
 
         <div class="main-content">
             <h1> Parent Registration </h1>
-            <?php 
-                if (isset($_SESSION['registration_success'])) {
-                    echo '<div class="alert alert-success">' . $_SESSION['registration_success'] . '</div>';
-                    unset($_SESSION['registration_success']);
-                }
-                if (isset($_SESSION['registration_error'])) {
-                    echo '<div class="alert alert-error">' . $_SESSION['registration_error'] . '</div>';
-                    unset($_SESSION['registration_error']);
-                }
+            <?php
+            if (isset($_SESSION['registration_success'])) {
+                echo '<div class="alert alert-success">' . $_SESSION['registration_success'] . '</div>';
+                unset($_SESSION['registration_success']);
+            }
+            if (isset($_SESSION['registration_error'])) {
+                echo '<div class="alert alert-error">' . $_SESSION['registration_error'] . '</div>';
+                unset($_SESSION['registration_error']);
+            }
             ?>
             <div class="form-container">
                 <form id="parent-account-registration-form" action="includes/register-process.php" method="POST">
@@ -76,6 +76,16 @@ require_once "../includes/sidebar.php";
 
                     <button type="submit" type="register-button">REGISTER</button>
                 </form>
+                <?php
+                if (isset($_SESSION['registration_error'])) {
+                    echo '<div class="error">' . $_SESSION['registration_error'] . '</div>';
+                    unset($_SESSION['registration_error']);
+                }
+                if (isset($_SESSION['registration_success'])) {
+                    echo '<div class="success">' . $_SESSION['registration_success'] . '</div>';
+                    unset($_SESSION['registration_success']);
+                }
+                ?>
             </div>
         </div>
     </div>
